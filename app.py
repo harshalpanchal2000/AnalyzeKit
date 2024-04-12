f42a20c05cf3ca6450e793f9129ec0a0f1f21c2a
Harshal Panchal <78840044+harshalpanchal2000@users.noreply.github.com>
2024-04-12 11:12:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ warnings.filterwarnings("ignore")
 page_bg_img = """
 <style>
 [data-testid="stAppViewContainer"]{
-background-image: URL("https://images.unsplash.com/photo-1604076947037-d909e2f10fca?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+background-image:  url("https://images.unsplash.com/photo-1604076947037-d909e2f10fca?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
 background-size: cover;
 }
 </style>
