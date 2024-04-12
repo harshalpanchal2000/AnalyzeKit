bb63c9036ef35eb1f65df7e0ec75905dabacecb3
Harshal Panchal <78840044+harshalpanchal2000@users.noreply.github.com>
2024-04-11 23:06:39 -0400

    Update numerical_analysis.py

--- code/numerical_analysis.py
+++ code/numerical_analysis.py
@@ -1,8 +1,7 @@
+import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from scipy.stats import skew, kurtosis
-from sklearn.preprocessing import StandardScaler
 
 # Function to visualize distribution
 def visualize_data_distribution(df, column):
@@ -11,12 +10,12 @@ def visualize_data_distribution(df, column):
     plt.title(f'Distribution of {column}')
     plt.xlabel(column)
     plt.ylabel('Frequency')
-    plt.show()
+    st.pyplot()
 
 # Function to visualize pairwise relationships
 def visualize_pairwise_relationships(df):
     sns.pairplot(df)
-    plt.show()
+    st.pyplot()
 
 # Function to visualize feature relationships
 def visualize_feature_relationships(df, column_x, column_y):
@@ -25,20 +24,24 @@ def visualize_feature_relationships(df, column_x, column_y):
     plt.title(f'Relationship between {column_x} and {column_y}')
     plt.xlabel(column_x)
     plt.ylabel(column_y)
-    plt.show()
+    st.pyplot()
 
 # Function to visualize correlation
 def visualize_correlation(df):
     plt.figure(figsize=(10, 6))
     sns.heatmap(df.corr(), annot=True, cmap='coolwarm', fmt=".2f")
     plt.title('Correlation Heatmap')
-    plt.show()
+    st.pyplot()
 
 # Function for numerical analysis
 def perform_numerical_analysis(df):
     numerical_columns = df.select_dtypes(include=['int', 'float']).columns
+    st.subheader("Numerical Features:")
+    st.write(numerical_columns)
     for column in numerical_columns:
-        print(f"Analysis for {column}:")
+        st.subheader(f"Analysis for {column}:")
+        st.write("Sample Values:")
+        st.write(df[column].sample(min(5, df.shape[0])))
         visualize_data_distribution(df, column)
         visualize_feature_relationships(df, column, df.columns[-1])  # Assuming last column is the target variable
         visualize_pairwise_relationships(df)
