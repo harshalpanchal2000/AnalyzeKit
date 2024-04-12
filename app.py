11e971b05703ebfc0001295d54ff924ea2c72fd5
Harshal Panchal <78840044+harshalpanchal2000@users.noreply.github.com>
2024-04-12 11:11:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,8 @@ warnings.filterwarnings("ignore")
 page_bg_img = """
 <style>
 [data-testid="stAppViewContainer"]{
-background-color: #ffffff;
-opacity: 0.6;
-background-image:  repeating-radial-gradient( circle at 0 0, transparent 0, #e5e5f7 10px ), 
-repeating-linear-gradient( #fcdc5c55, #fcdc5c )
+background-image: URL("https://images.unsplash.com/photo-1604076947037-d909e2f10fca?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+background-size: cover;
 }
 </style>
 """
