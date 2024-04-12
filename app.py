0d606940a0fc34ef203ee22166d02941a3811613
Harshal Panchal <78840044+harshalpanchal2000@users.noreply.github.com>
2024-04-12 01:16:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def main():
 
     # File uploader
     st.sidebar.title("Upload Dataset")
-    uploaded_file = st.sidebar.file_uploader("",type=["csv"])
+    uploaded_file = st.sidebar.file_uploader("", type=["csv"])
 
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
@@ -26,22 +26,14 @@ def main():
         analysis_option = st.sidebar.radio("Choose Analysis", ["Basic Analysis", "Categorical Analysis", "Numerical Analysis"])
 
         if analysis_option == "Basic Analysis":
+            st.markdown("## Basic Analysis")
             perform_basic_analysis(df)
         elif analysis_option == "Categorical Analysis":
-            categorical_columns = df.select_dtypes(include=['object', 'category']).columns
-            if len(categorical_columns) == 0:
-                st.write("No categorical features found in the dataset.")
-            else:
-                perform_categorical_analysis(df)
+            st.markdown("## Categorical Analysis")
+            perform_categorical_analysis(df)
         elif analysis_option == "Numerical Analysis":
-            numerical_columns = df.select_dtypes(include=['int', 'float']).columns
-            if len(numerical_columns) == 0:
-                st.write("No numerical features found in the dataset.")
-            else:
-                perform_numerical_analysis(df)
-
-
-    st.markdown('<link rel="stylesheet" href="styles.css">', unsafe_allow_html=True)
+            st.markdown("## Numerical Analysis")
+            perform_numerical_analysis(df)
 
     # Link to LinkedIn profile with yellow-colored text
     st.markdown(
